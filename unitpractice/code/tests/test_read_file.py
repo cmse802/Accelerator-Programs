b037d35ead4985cf78aacaac85ec96e62013aeaa
Kelly Anderson <anderske@frib.msu.edu>
2020-10-23 21:42:44 -0400

    Tried to fix lack of PYTHONPATH variable

--- unitpractice/code/tests/test_read_file.py
+++ unitpractice/code/tests/test_read_file.py
@@ -1,16 +1,20 @@
 import read_file
 import pytest
 import numpy as np
+import sys
+
+sys.path.insert(1, '../../')
+sys.path.insert(1, '../')
+
 def test_file_load_good():
 	# test if function returns numpy array for good input file.
-	data = read_file.read_file("input_good.inp")
+	data = read_file.read_file("../../input_good.inp")
 	assert type(data) == np.ndarray 
 def test_file_load_bad():
 	# Test if the function raises the exception if nonexisting file name is passed as 
 	# parameter.
 	with pytest.raises(ValueError) as excinfo:
-		read_file.read_file("input_bad.inp") 
-	
+		read_file.read_file("../../input_bad.inp")
 	assert "enough rows in input file" in str(excinfo.value)
 
 def test_file_load_missing_file():
