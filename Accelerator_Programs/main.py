15ac501cd21cb0f2c19b95574d1537fe23dd2dc8
Kelly Anderson <anderske@frib.msu.edu>
2020-11-06 15:22:33 -0500

    Did 1106 linting project

--- Accelerator_Programs/main.py
+++ Accelerator_Programs/main.py
@@ -1,21 +1,24 @@
 import numpy as np
 
+
 def madx_out(lat, file):
-	"""Reads in the lattice to madx and runs the file in madx. Returns a matrix of info about the trial. TEST CHANGE"""
-	
-	results = np.zeros((2,2))
-	
-	return results
+    """Reads in the lattice to madx and runs the file in madx. Returns a matrix of info about the trial."""
+
+    results = np.zeros((2, 2))
+
+    return results
+
 
 class learn(object):
-	"""Takes in madx results and begins the process of finding a model to predict them"""
-	def __init__(self, results, init):
-		self.results = results
-		self.init = init
-	
-	def model(self, init):
-		"""The model takes initial condition and predicts results based on the learning"""
+    """Takes in madx results and begins the process of finding a model to predict them"""
 
-		new_results = np.zeros((2,2))
-		
-		return new_results
+    def __init__(self, results, init):
+        self.results = results
+        self.init = init
+
+    def model(self, init):
+        """The model takes initial condition and predicts results based on the learning"""
+
+        new_results = np.zeros((2, 2))
+
+        return new_results
--- Accelerator_Programs/tests/test_read_file.py
+++ Accelerator_Programs/tests/test_read_file.py
@@ -1,22 +1,25 @@
-from Accelerator_Programs import read_file
 import pytest
 import numpy as np
-import sys
+from Accelerator_Programs import read_file
+
 
 def test_file_load_good():
-	# test if function returns numpy array for good input file.
-	data = read_file.read_file("./Example_data/input_good.inp")
-	assert type(data) == np.ndarray 
+    # test if function returns numpy array for good input file.
+    data = read_file.read_file("./Example_data/input_good.inp")
+    assert type(data) == np.ndarray
+
+
 def test_file_load_bad():
-	# Test if the function raises the exception if nonexisting file name is passed as 
-	# parameter.
-	with pytest.raises(ValueError) as excinfo:
-		read_file.read_file("./Example_data/input_bad.inp")
-	assert "enough rows in input file" in str(excinfo.value)
+    # Test if the function raises the exception if nonexisting file name is passed as
+    # parameter.
+    with pytest.raises(ValueError) as excinfo:
+        read_file.read_file("./Example_data/input_bad.inp")
+    assert "enough rows in input file" in str(excinfo.value)
+
 
 def test_file_load_missing_file():
-	# Test if the function raises the exception if nonexisting file name is passed as 
-	# parameter.
-	with pytest.raises(ValueError) as excinfo:
-		read_file.read_file("./Example_data/non-existing_file.txt")
-	assert "file does not exist" in str(excinfo.value)
+    # Test if the function raises the exception if nonexisting file name is passed as
+    # parameter.
+    with pytest.raises(ValueError) as excinfo:
+        read_file.read_file("./Example_data/non-existing_file.txt")
+    assert "file does not exist" in str(excinfo.value)
